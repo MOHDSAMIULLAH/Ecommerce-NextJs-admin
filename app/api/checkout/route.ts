77564f4d9782d0452c76421825047ef758b43255
MOHD SAMIULLAH <samiullah0813@gmail.com>
2024-05-15 15:34:40 +0530

    cors

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -4,7 +4,7 @@ import { stripe } from "@/lib/stripe";
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
   "Access-Control-Allow-Credentials": "true",
 };
 
